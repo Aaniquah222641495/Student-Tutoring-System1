926c20b7b650fe2954d105e28003a77ebfc607e0
SAVHPHO3NIX <127983823+SAVHPHO3NIX@users.noreply.github.com>
2024-05-12 23:24:38 +0200

    updates

--- src/main/java/za/ac/cput/service/Calendar_Entry_Service.java
+++ src/main/java/za/ac/cput/service/Calendar_Entry_Service.java
@@ -9,4 +9,36 @@ import za.ac.cput.repository.Calendar_Entry_Repository;
 public class Calendar_Entry_Service implements IService<Calendar_Entry, Integer>{
     private final Calendar_Entry_Repository repository;
 
+    @Autowired
+    public Calendar_Entry_Service(Calendar_Entry_Repository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Calendar_Entry create(Calendar_Entry object) {
+        return repository.save(object);
+    }
+
+    @Override
+    public Calendar_Entry read(Integer integer) {
+        return repository.findById(integer).orElse(null);
+    }
+
+    @Override
+    public Calendar_Entry update(Calendar_Entry object) {
+        Integer id = object.getEntry_id();
+        if (id != null && repository.existsById(id)) {
+            return repository.save(object);
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Integer integer) {
+        try {
+            repository.deleteById(integer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
